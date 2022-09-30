63fa749eb73159a08f29bf64823a3fa81d650648
不祈祷 <2657040829@qq.com>
2022-09-30 12:16:53 +0800

    okok

--- src/main/java/com/example/demo/controller/FileController.java
+++ src/main/java/com/example/demo/controller/FileController.java
@@ -3,14 +3,20 @@ package com.example.demo.controller;
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.util.IdUtil;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.crypto.SecureUtil;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.example.demo.entity.Files;
+import com.example.demo.mapper.FileMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
+import java.net.URLEncoder;
 import java.util.List;
 
 import com.example.demo.service.IFileService;
@@ -33,38 +39,103 @@ public class FileController {
     @Value("${files.upload.path}")
     private String fileUploadPath;
 
+    @Resource
+    private FileMapper fileMapper;
+
     /**
      * 文件上传接口
+     *
      * @param file 前端传递过来的文件
      * @return
      * @throws IOException
      */
-
     @PostMapping("/upload")
     public String upload(@RequestParam MultipartFile file) throws IOException {
         String originalFilename = file.getOriginalFilename();
         String type = FileUtil.extName(originalFilename);
         long size = file.getSize();
 
-        // 先存储到磁盘
-        File uploadParentFile = new File(fileUploadPath);
-
-        // 判断配置文件目录是否存在，若不存在则创建一个新的文件目录
-        if (!uploadParentFile.exists()) {
-            uploadParentFile.mkdirs();
-        }
-
         // 定义一个文件唯一的标识码
         String uuid = IdUtil.fastSimpleUUID();
-        File uploadFile = new File(fileUploadPath + uuid + StrUtil.DOT + type);
-        // 把获取到的文件存储到磁盘目录
-        file.transferTo(uploadFile);
+        String fileUuid = uuid + StrUtil.DOT + type;
+
+        File uploadFile = new File(fileUploadPath + fileUuid);
+        // 判断配置文件目录是否存在，若不存在则创建一个新的文件目录
+        File parentFile = uploadFile.getParentFile();
+        if (!parentFile.exists()) {
+            parentFile.mkdirs();
+        }
+
+        // 当文件存在的时候在获取文件的md5
+        String md5, url;
+        if (uploadFile.exists()) {
+            // 获取文件的md5, 通过对比md5避免重复上传相同内容的文件
+            md5 = SecureUtil.md5(uploadFile);
+            //  从数据库查询是否有相同的记录
+            Files dbFiles = getFileByMd5(md5);
+
+            // 获取文件的url
+            if (dbFiles != null) {
+                url = dbFiles.getUrl();
+            } else {
+                // 把获取到的文件存储到磁盘目录
+                file.transferTo(uploadFile);
+                url = "http://localhost:9090/file/" + fileUuid;
+            }
+        } else {
+            // 把获取到的文件存储到磁盘目录
+            file.transferTo(uploadFile);
+            // 直接获取md5
+            md5 = SecureUtil.md5(uploadFile);
+            url = "http://localhost:9090/file/" + fileUuid;
+        }
 
         // 存储数据库
+        Files saveFile = new Files();
+        saveFile.setName(originalFilename);
+        saveFile.setType(type);
+        saveFile.setSize(size / 1024); // B => KB
+        saveFile.setUrl(url);
+        saveFile.setMd5(md5);
+        fileMapper.insert(saveFile);
 
+        return url;
+    }
 
+    /**
+     * 文件下载接口
+     * 导出通过流导出到网页中
+     *
+     * @param fileUuid
+     * @param response
+     * @throws IOException
+     */
+    @GetMapping("/{fileUuid}")
+    public void download(@PathVariable String fileUuid, HttpServletResponse response) throws IOException {
+        // 根据文件的唯一标识码 获取文件
+        File uploadFile = new File(fileUploadPath + fileUuid);
+        // 设置输出流的格式
+        ServletOutputStream os = response.getOutputStream();
+        response.addHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(fileUuid, "UTF-8"));
+        response.setContentType("application/octet-stream");
 
-        return "";
+        // 读取上传的字节流
+        os.write(FileUtil.readBytes(uploadFile));
+        os.flush();
+        os.close();
+    }
+
+    /**
+     * 通过文件的md5查询文件
+     *
+     * @param md5
+     * @return
+     */
+    private Files getFileByMd5(String md5) {
+        // 查询文件的md5是否存在
+        QueryWrapper<Files> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("md5", md5);
+        return fileMapper.selectOne(queryWrapper);
     }
 
 }
